b36c66b1c378f3576578bae6c779dcdfef7746ee
toban@dfds.com <toban@dfds.com>
2020-02-18 15:38:39 +0100

    Playing around with different ES versions to test limitations of Mocha/Chai/Karma combo.

--- src/Minions.Core.Test/src/simple.test.ts
+++ src/Minions.Core.Test/src/simple.test.ts
@@ -1,11 +1,11 @@
-import { WebComponent } from "minions.core/components/WebComponent";
+import { WebComponent } from "minions-core/lib/components/WebComponent";
 
 const expect: any = chai.expect;
 
 suite("Minions.WebComponent", () => {
     test("should return true", () => {
         var result = WebComponent;
-
+        
         expect(result).to.equal(result);
     });
 });
