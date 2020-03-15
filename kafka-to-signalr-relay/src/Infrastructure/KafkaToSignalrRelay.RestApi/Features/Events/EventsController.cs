b5958afc50064d410db1cff5419b666ff55026ff
Kim Lindhard <kilin@dfds.com>
2020-03-15 14:57:19 +0100

    rest api can echo post on /evets endpoint

---
+++ kafka-to-signalr-relay/src/Infrastructure/KafkaToSignalrRelay.RestApi/Features/Events/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaToSignalrRelay.RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        public EventsController()
+        {
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> AddEvent([FromBody] object @event)
+        {
+            Console.WriteLine(@event);
+
+            return Ok(@event);
+        }
+    }
+}
