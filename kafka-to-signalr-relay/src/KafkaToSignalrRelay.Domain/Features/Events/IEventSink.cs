363894ec15014cde896a91d7761653c17a81c1e9
toban@dfds.com <toban@dfds.com>
2020-03-26 19:10:15 +0100

    =removing-old-code

--- kafka-to-signalr-relay/src/KafkaToSignalrRelay.Domain/Features/Events/IEventSink.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace KafkaToSignalrRelay.Domain.Features.Events
-{
-    public interface IEventSink
-    {
-        Task ReceiveEventAsync(object @event);
-    }
-}
